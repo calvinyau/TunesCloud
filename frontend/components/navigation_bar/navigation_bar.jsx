c421eddb09e879287b8c215067655cf732d44df9
Calvin Yau <calvinj.yau@gmail.com>
2017-01-12 13:56:49 -0800

    Added switching login/logout buttons

--- frontend/components/navigation_bar/navigation_bar.jsx
+++ frontend/components/navigation_bar/navigation_bar.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const sessionLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login" activeClassName="current" className="login-button">Login</Link>
-    &nbsp;or&nbsp;
-    <Link to="/signup" activeClassName="current" className="signup-button">Sign up!</Link>
-  </nav>
-);
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-const personalGreeting = (currentUser, logout) => (
-	<div className="header-group">
-    <h2 className="header-name">Hi, {currentUser.username}!</h2>
-    <button className="header-button" onClick={logout}>Log Out</button>
-	</div>
-);
+  render() {
+    const header = () => {
+      return this.props.currentUser ? (
+      <div className="header-group">
+        <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
+        <button className="header-button" onClick={this.props.logout}>Log Out</button>
+      </div>) : (<nav className="login-signup">
+        <Link to="/login" activeClassName="current" className="login-signup-button button">Login</Link>
+        &nbsp;or&nbsp;
+        <Link to="/signup" activeClassName="current" className="login-signup-button button">Sign up!</Link>
+      </nav>);
+    };
 
-const NavigationBar = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+    return (
+      <div>
+        <header className="header">
+          <Link to="/" className="home-button button">TUNESCLOUD</Link>
+          {header()}
+        </header>
+      </div>
+    )
+  }
+}
 
 export default NavigationBar;
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -6,6 +6,7 @@ class SessionForm extends React.Component {
 		super(props);
 		this.state = { username: "", password: "" };
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.demoLogin = this.demoLogin.bind(this);
 	}
 
 	componentDidUpdate() {
@@ -28,6 +29,15 @@ class SessionForm extends React.Component {
 		e.preventDefault();
 		const user = this.state;
 		this.props.processForm({user});
+	}
+
+	demoLogin() {
+	  this.setState({
+	    username: "test",
+	    password: "123456"
+	  });
+	  const user = this.state;
+	  this.props.login(user);
 	}
 
 	navLink() {
@@ -57,35 +67,38 @@ class SessionForm extends React.Component {
 	render() {
     let form_title = this.toTitleCase(this.props.formType);
 		return (
-			<div className="login-form-container">
-				<form onSubmit={this.handleSubmit} className="login-form-box">
-					Welcome to TunesCloud!
-					<br/>
-          <div className="form-title">
-            <h4>{form_title}</h4>
-          </div>
-					{/* <!-- Please {this.props.formType} or {this.navLink()} --> */}
-					{this.renderErrors()}
-					<div className="login-form">
+      <div className="body-container">
+        <div className="login-form-container">
+					<form onSubmit={this.handleSubmit} className="login-form-box">
+						Welcome to TunesCloud!
 						<br/>
-						<label> Username:
-							<input type="text"
-								value={this.state.username}
-								onChange={this.update("username")}
-								className="login-input" />
-						</label>
-						<br/>
-						<label> Password:
-							<input type="password"
-								value={this.state.password}
-								onChange={this.update("password")}
-								className="login-input" />
-						</label>
-						<br/>
-						<input type="submit" value="Submit" className="login-submit"/>
-					</div>
-				</form>
-			</div>
+						<div className="form-title">
+							<h4>{form_title}</h4>
+						</div>
+						{/* <!-- Please {this.props.formType} or {this.navLink()} --> */}
+						{this.renderErrors()}
+						<div className="login-form">
+							<br/>
+							<label> Username:
+								<input type="text"
+									value={this.state.username}
+									onChange={this.update("username")}
+									className="login-input" />
+							</label>
+							<br/>
+							<label> Password:
+								<input type="password"
+									value={this.state.password}
+									onChange={this.update("password")}
+									className="login-input" />
+							</label>
+							<br/>
+							<input type="submit" value="Submit" className="login-submit"/>
+							<button onClick={this.demoLogin} className="demo-button">Demo Login</button>
+						</div>
+					</form>
+        </div>
+      </div>
 		);
 	}
 
