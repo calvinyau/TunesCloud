fed06f1b1a25c75e3bc45ce3e61d35fba0f63db3
Calvin Yau <calvinj.yau@gmail.com>
2017-01-19 12:20:19 -0800

    Create comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,17 @@
+class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :track_id, :user_id)
+  end
+end
