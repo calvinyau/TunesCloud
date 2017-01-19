b6728a1e4ba138cf291680d17c48707ea53e2d86
Calvin Yau <calvinj.yau@gmail.com>
2017-01-18 17:52:15 -0800

    Create link to track show page from track item title

--- frontend/components/tracks/track_item.jsx
+++ frontend/components/tracks/track_item.jsx
@@ -20,16 +20,15 @@ class TrackItem extends React.Component {
 
   render() {
     const trackItem = this.props.track;
-    let trackArtwork = trackItem.artwork_url;
-    if (trackArtwork === "") {
-      trackArtwork = "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484354963/headphones_vw9muh.jpg"
-    }
+    const trackArtwork = trackItem.artwork_url === "" ? trackItem.artwork_url :
+      "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484354963/headphones_vw9muh.jpg";
     let playPauseIcon;
     if (this.props.playing) {
       playPauseIcon = this.props.track.name === this.props.currentTrack.name ? "fa fa-pause-circle fa-3x" : "fa fa-play-circle fa-3x"
     } else {
       playPauseIcon = "fa fa-play-circle fa-3x";
     }
+    const trackUrl = `/tracks/${this.props.track.id}`;
 
     return (
       <div className="track-item">
@@ -44,7 +43,9 @@ class TrackItem extends React.Component {
             </span>
             <br />
             <span className="track-item-detail-title">
-              {trackItem.name}
+              <Link to={trackUrl}>
+                {trackItem.name}
+              </Link>
             </span>
           </div>
         </div>
