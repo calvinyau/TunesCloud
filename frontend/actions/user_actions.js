c64f2a2a72359d5cd27fd025aa1ee3b18956e1cd
Calvin Yau <calvinj.yau@gmail.com>
2017-01-19 01:09:26 -0800

    Forgot to import user api util to user actions

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,3 +1,5 @@
+import * as APIUtil from '../util/user_api_util';
+
 export const RECEIVE_USERS = "RECEIVE_USERS";
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_USER_ERRORS = 'RECEIVE_USER_ERRORS';
