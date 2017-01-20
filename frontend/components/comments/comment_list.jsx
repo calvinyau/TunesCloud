5e6d4fc4063c8fe691a672a18d871ac4916005bb
Calvin Yau <calvinj.yau@gmail.com>
2017-01-19 17:15:01 -0800

    Users are not passed to comment list; using debugger

--- frontend/components/comments/comment_list.jsx
+++ frontend/components/comments/comment_list.jsx
@@ -9,10 +9,12 @@ class CommentList extends React.Component {
 
 
   render() {
+    let users = this.props ? this.props.users : [];
+    debugger;
     const list =
     this.props.comments.map((comment, index) => {
       let commenter;
-      commenter = this.props.users ? this.props.users[comment.user_id] : {};
+      commenter = users ? users[comment.user_id] : {};
       return (
         <li className="comment-list-item" key={index}>
           <CommentItem body={comment.body} commenter={commenter}/>
