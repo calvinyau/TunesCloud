ab1e3c5989651e4b6362f0c1af331e799d799ac1
Calvin Yau <calvinj.yau@gmail.com>
2017-01-20 16:02:14 -0800

    Add production readme, change seeds

--- README.md
+++ README.md
@@ -1,24 +1,63 @@
-# README
+# TunesCloud
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+[TunesCloud Live][sitelink]
+[sitelink]: https://tunescloud.herokuapp.com
 
-Things you may want to cover:
+TunesCloud is a music streaming web application inspired by SoundCloud. Users can create an account to upload tracks, listen to their own as well as other users' content and leave their thoughts.
 
-* Ruby version
+## Features
 
-* System dependencies
+- User accounts w/ authentication
+- Creating music tracks w/ artwork
+- Playing music without interruption
+- Creating comments on tracks
+- Viewing user profiles
+- Viewing track profiles
 
-* Configuration
+## Design
 
-* Database creation
+The component hierarchy, a sample of the state, the schema, API endpoints, and initial wireframe mockups can be found here:
 
-* Database initialization
+[Component Hierarchy][components]
 
-* How to run the test suite
+[Sample State][state]
 
-* Services (job queues, cache servers, search engines, etc.)
+[Schema][schema]
 
-* Deployment instructions
+[API Endpoints][endpoints]
 
-* ...
+[components]: ./docs/component-hierarchy.md
+[state]: ./docs/sample-state.md
+[schema]: ./docs/schema.md
+[endpoints]: ./docs/api-endpoints.md
+
+[Initial Wireframes][wireframes]
+[wireframes]: ./docs/wireframes
+
+## Technology Stack
+
+TunesCloud is a single-page web application built on Ruby on Rails for the backend and React.js in combination with Redux for the frontend.
+
+#### Backend
+Hosting: Heroku
+
+Database: PostgreSQL RDBMS
+
+File Hosting: Cloudinary
+
+#### Frontend
+
+Frontend Dependencies: npm
+
+File Bundling: Webpack
+
+API Calls: jQuery AJAX
+
+Javascript Utility Library: Lodash
+
+File Player: react-player-container
+
+## Future Features
+- Wave Forms
+- Playlists
+- Likes
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,8 +38,6 @@ avatar_urls = [
   })
 end
 
-track0 = Track.create({name: "Daft Punk - Give Life Back to Music", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484788011/Give_Life_Back_to_Music_rwqv9n.mp3",
-   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484788177/Random_Access_Memories_izpzws.jpg", user_id: (Random.rand(24) + 1)})
 track1 = Track.create({name: "Christina Perri - Jar of Hearts", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484470383/Christina_Perri_-_Jar_of_Hearts_kzuhyf.mp3",
    artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484611075/jar-of-hearts-album-cover_tlydxv.png", user_id: (Random.rand(24) + 1)})
 track2 = Track.create({name: "Justin Timberlake - Mirrors", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484470392/Justin_Timberlake_-_Mirrors_wixzs6.mp3",
@@ -58,8 +56,8 @@ track8 = Track.create({name: "Passion Pit - Take a Walk", track_url: "http://res
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484788177/passion_pit_take_a_walk_dbvcnx.jpg", user_id: (Random.rand(24) + 1)})
 track9 = Track.create({name: "Frank Ocean - Thinking About You", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484788884/Frank_Ocean_-_Thinking_About_You_ybc2fd.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484703806/channel_orange.jpg", user_id: (Random.rand(24) + 1)})
-track10 = Track.create({name: "American Authors - Believer", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942127/Tracks/American_Authors_-_Believer.mp3",
-  artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484943369/American_Authors_Believer_artwork_mxszzl.jpg", user_id: (Random.rand(24) + 1)})
+
+
 track11 = Track.create({name: "Bastille - Pompeii", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942423/Tracks/Bastille_-_Pompeii.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484943369/bastille_pompeii_artwork_jumxox.jpg", user_id: (Random.rand(24) + 1)})
 track12 = Track.create({name: "Bastille - Things We Lost in the Fire", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942192/Tracks/Bastille_-_Things_We_Lost_in_the_Fire.mp3",
@@ -72,14 +70,19 @@ track15 = Track.create({name: "Lupe Fiasco - Old School Love ft. Ed Sheeran", tr
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484943370/lupefiasco-oldschoollove_yaxt8r.jpg", user_id: (Random.rand(24) + 1)})
 track16 = Track.create({name: "MKTO - Classic", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942120/Tracks/MKTO_-_Classic.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484943371/MKTO_Classic_artwork_csw7dy.png", user_id: (Random.rand(24) + 1)})
-track17 = Track.create({name: "OneRepublic - Feel Again", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942130/Tracks/OneRepublic_-_Feel_Again.mp3",
-  artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484948837/onerepublic_feel_again_artwork_cyjchn.png", user_id: (Random.rand(24) + 1)})
+
 track18 = Track.create({name: "OneRepublic - Secrets", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942421/Tracks/OneRepublic_-_Secrets.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484948827/OneRepublic_Secrets_oxmsea.jpg", user_id: (Random.rand(24) + 1)})
 track19 = Track.create({name: "Sia - Chandelier", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942455/Tracks/Sia_-_Chandelier.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484948916/Sia_Chandelier_xbsvkv.png", user_id: (Random.rand(24) + 1)})
 track20 = Track.create({name: "The Neighborhood - Sweater Weather", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942395/Tracks/The_Neighbourhood_-_Sweater_Weather.mp3",
   artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484949102/sweater_weather_artwork_upgevb.jpg", user_id: (Random.rand(24) + 1)})
+  track17 = Track.create({name: "OneRepublic - Feel Again", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942130/Tracks/OneRepublic_-_Feel_Again.mp3",
+    artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484948837/onerepublic_feel_again_artwork_cyjchn.png", user_id: (Random.rand(24) + 1)})
+track10 = Track.create({name: "American Authors - Believer", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484942127/Tracks/American_Authors_-_Believer.mp3",
+  artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484943369/American_Authors_Believer_artwork_mxszzl.jpg", user_id: (Random.rand(24) + 1)})
+track21 = Track.create({name: "Daft Punk - Give Life Back to Music", track_url: "http://res.cloudinary.com/dsvfpq1b7/video/upload/v1484788011/Give_Life_Back_to_Music_rwqv9n.mp3",
+  artwork_url: "http://res.cloudinary.com/dsvfpq1b7/image/upload/v1484788177/Random_Access_Memories_izpzws.jpg", user_id: (Random.rand(24) + 1)})
 # track21 = Track.create({name: "", track_url: "",
 #   artwork_url: "", user_id: (Random.rand(24) + 1)})
 # track22 = Track.create({name: "", track_url: "",
