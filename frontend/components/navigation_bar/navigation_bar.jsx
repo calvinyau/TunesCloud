eee231a23016d505bccbd394c986bc90a07476ac
Calvin Yau <calvinj.yau@gmail.com>
2017-02-11 07:07:34 -0800

    Update demo user login username

--- frontend/components/navigation_bar/navigation_bar.jsx
+++ frontend/components/navigation_bar/navigation_bar.jsx
@@ -11,7 +11,7 @@ class NavigationBar extends React.Component {
   demoLogin() {
     const demoUser = {
       user: {
-        username: "Demo",
+        username: "Demo User",
         password: "123456"
       }
     };
